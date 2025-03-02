f09722cc338383fe91ad2836eacc54e519db03e7
xSarscov <vargasernest7@gmail.com>
2025-03-02 13:45:21 -0600

    Modify the field validation middleware to change the structure of the error message

--- middlewares/fieldsValidator.js
+++ middlewares/fieldsValidator.js
@@ -7,7 +7,7 @@ const fieldsValidator = (req, res = response, next) => {
     if (!errors.isEmpty()) {
         return res.status(400).json({
             ok: false,
-            errors: errors.mapped()
+            msg: errors.array()
         })
     }
 
