7548bb4063875feb8f41a5dfcb4cbefb98de051d
xSarscov <vargasernest7@gmail.com>
2025-03-02 18:29:16 -0600

    Add Path Handling to Serve the index.html File on All Unhandled Requests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./database/config');
 require('dotenv').config();
 
@@ -16,7 +17,11 @@ app.use( express.json() );
 
 app.use('/api/auth', require('./routes/auth'));
 
-app.use('/api/events', require('./routes/events'))
+app.use('/api/events', require('./routes/events'));
+
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en puerto 4000')
